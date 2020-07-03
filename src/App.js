0afe07129f4e7d39ad71425ed8be0f0e2b3ebad3
tranhiepnguyenhuy1999 <tranhiepnguyenhuy1999@gmail.com>
2020-07-03 23:19:31 +0700

    some fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import './App.css';
 import Footer from './component/Footer/index'
-import Header from './component/Header'; 
+import Header from './component/Header';
+import NotFound from './pages/NotFound'
 const  Photo = React.lazy(()=> import('./features/Photo/index')) 
 
 function App(){
@@ -13,8 +14,9 @@ function App(){
       <Router>
         <Header></Header>
         <Switch>
-          {/* <Redirect from='/' to='/photo'></Redirect> */}
-          <Route path='/photo' component={Photo}/>
+          <Redirect from='/' to='/photo'></Redirect>
+          <Route exact path='/photo' component={Photo}/>
+          <Route component={NotFound}></Route>
         </Switch>
       </Router>
       <Footer></Footer>
--- src/features/Photo/index.js
+++ src/features/Photo/index.js
@@ -6,15 +6,13 @@ import Detail from '../../pages/Detail';
 function Photo() {
     const match = useRouteMatch();
     return (
-        <div>  
             <Switch>
                 <Route exact path='/photo' component={MainPage}></Route>
-                <Route   exact path={`${match.url}/add`} component={AddPhoto}/>
-                <Route  exact path={`${match.url}/:idphoto`} component={AddPhoto}/>
+                <Route exact path={`${match.url}/add`} component={AddPhoto}/>
+                <Route exact path={`${match.url}/:idphoto`} component={AddPhoto}/>
                 <Route exact path={`${match.url}/detail/:id`} component={Detail}></Route>
                 <Route/>
             </Switch>
-        </div>
     )
 }
 export default Photo
