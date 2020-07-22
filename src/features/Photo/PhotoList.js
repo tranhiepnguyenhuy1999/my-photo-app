92dd0943270bf69f93df943e2750a181adf494a8
tranhiepnguyenhuy1999 <tranhiepnguyenhuy1999@gmail.com>
2020-07-22 21:42:10 +0700

    try new version for filter feature
    change the old version with filter and data from redux separate. Now I combine all in one datastate and just change on it

--- src/features/Photo/PhotoList.js
+++ src/features/Photo/PhotoList.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import PhotoItem from './PhotoItem';
 import {useSelector, useDispatch} from 'react-redux';
@@ -9,51 +9,41 @@ import Pagination from '../../component/PaginationPage'
 
 function PhotoList() {
     const photoStore = useSelector(state => state.photoStore);
+    const [data, setData]=useState([]);
+    useEffect(() => {
+        setData(photoStore)
+    }, [photoStore]);
     const dispatch = useDispatch();
-    const [onFilter, setOnFilter]=useState(false)
-    const [pageNum, setPageNum]=useState(1)
-    const [pageNumFil, setPageNumFil]=useState(1)
-    const [filter, setfilter] =useState([])
-    const [reAll, setreAll]=useState([1])
+    const [pageNum, setPageNum]=useState(1);
+
+
     // Remove photo from Redux
     const onClickRemove=(id)=>{
-        const action=removePhoto(id)
-        dispatch(action)
-        setfilter(photoStore.filter(photo=>photo.id !== id))
+        const action=removePhoto(id);
+        dispatch(action);
     }
     // Filter photos by type
     const handelFilter=(values)=>{
         if(values.value===4)
         {
-            setOnFilter(false)
+            setData(photoStore);
             return;
         }
-        setOnFilter(true)
-        setfilter(photoStore.filter(photo=>photo.type===values.value))
+        setData(photoStore.filter(photo=>photo.type===values.value));
     }
-    // Pagination for All type
+    // Pagination 
     const indexEnd= pageNum*8;
     const indexStart=indexEnd-8;
-    const pagePhoto= photoStore.slice(indexStart,indexEnd)
-    // Pagination Number for All type
-    const pagesNumber=Math.ceil(photoStore.length/8)
-    // Pagination for Filter type
-      const indexEndFilter= pageNumFil*8;
-      const indexStartFilter=indexEndFilter-8;
-      const pageFilterPhoto= filter.slice(indexStartFilter,indexEndFilter)
-    // Pagination Number for Filter type
-      const pagesFilterNumber=Math.ceil(filter.length/8)
+    const pagePhoto= data.slice(indexStart,indexEnd);
+    // Pagination 
+    const pagesNumber=Math.ceil(photoStore.length/8);
     // ChangePage for all
     const onClickPaggination=(num)=>{
-        setPageNum(num)
-    }
-    // Change page for filter
-    const onClickPagginationFilter=(num)=>{
-        setPageNumFil(num)
+        setPageNum(num);
     }
     const onRemoveAll=()=>{
         const action= removeAll();
-        dispatch(action)
+        dispatch(action);
     }
     return (
         <Container className="mt-3 bg-light mt-3">
@@ -63,28 +53,16 @@ function PhotoList() {
                 <Select options={OptionChoices} onChange={handelFilter} placeholder='All'></Select>
                     </Col>
             </Row>
-            {onFilter?
             <Col className='mb-3'>
-                {(filter.length===0)?<h1 >There is no photo in this type</h1>:<Row>
-                {pageFilterPhoto.map(photo => <PhotoItem
-                photo={photo}
-                removePhoto={onClickRemove}
-                />)}
-                </Row>
-                }
-            {filter.length>0 &&<Pagination pages={pagesFilterNumber} changePage={onClickPagginationFilter} pageCur={pageNumFil}></Pagination>}
-            </Col>
-            :
-            <Col className='mb-3'>
-                {(photoStore.length===0)?<h1>There is no photo in this store</h1>:<Row>
+                {(data.length===0)?<h1>There is no photo in this store</h1>:<Row>
                 {pagePhoto.map(photo => <PhotoItem
                 photo={photo}
                 removePhoto={onClickRemove}
                 />)}
             </Row>
             }
-            {photoStore.length>0 &&<Pagination pages={pagesNumber} changePage={onClickPaggination} pageCur={pageNum}></Pagination>}
-            </Col>}
+            {data.length>0 &&<Pagination pages={pagesNumber} changePage={onClickPaggination} pageCur={pageNum}></Pagination>}
+            </Col>
         
            
         </Container>
